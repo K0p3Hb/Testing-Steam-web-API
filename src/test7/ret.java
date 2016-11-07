1817d61ce9d9228709f39961d6cdc4bfdf3fe1b3
K0p3Hb <vlkornilo@gmail.com>
2016-11-07 20:12:36 +0500

    это тестовый стенд
    Создано после того как я модифицировал библиотеку реализовав класс для
    вещей .

---
+++ src/test7/ret.java
@@ -0,0 +1,83 @@
+package test7;
+
+
+import java.util.List;
+
+import de.inkvine.dota2stats.Dota2Stats;
+import de.inkvine.dota2stats.domain.GameMode;
+import de.inkvine.dota2stats.domain.MatchOverview;
+import de.inkvine.dota2stats.domain.filter.MatchHistoryFilter;
+import de.inkvine.dota2stats.domain.matchdetail.Item;
+import de.inkvine.dota2stats.domain.matchdetail.MatchDetail;
+import de.inkvine.dota2stats.domain.matchdetail.MatchDetailPlayer;
+import de.inkvine.dota2stats.domain.matchhistory.MatchHistory;
+import de.inkvine.dota2stats.domain.playersearch.PlayerSearchResult;
+import de.inkvine.dota2stats.exceptions.Dota2StatsAccessException;
+import de.inkvine.dota2stats.impl.Dota2StatsImpl;
+import java.sql.*;
+
+
+public class ret {
+	     
+	// Initialize without a proxy
+	
+	public static void main(String[] args) {
+		Dota2Stats stats = new Dota2StatsImpl("D796311CB1B7596E851E183264FAB02A");
+/*	try {
+	    List<PlayerSearchResult> results = stats.searchByPlayerName("john doe");
+
+	    // Look at the results!
+	    for(PlayerSearchResult item : results)
+	        System.out.println(item);   
+
+	} catch (Dota2StatsAccessException e) {
+	        // Do something if an error occured    
+	}*/
+
+	/*try {
+
+	    MatchHistory history = stats.getMostRecentMatchHistory();
+	    List<MatchOverview> overviews = history.getMatchOverviews();
+
+	    // print all match overviews found
+	    for (MatchOverview match : overviews)
+	        System.out.println(match);
+
+	} catch (Dota2StatsAccessException e1) {
+	    // Do something if an error occurs
+	}*/
+	
+	try {
+		//new MatchHistoryFilter().forDateMaximum(1);
+	    // Get the details for a given match id
+	    MatchDetail detail = stats.getMatchDetails(1762832017L);
+	   // System.out.println(detail);
+	    // extract all player stats from this game
+	    List<MatchDetailPlayer> playersStatsOfTheMatch = detail.getPlayers();
+
+	    // Show 'em
+	    for(MatchDetailPlayer player : playersStatsOfTheMatch) 
+	    	for(Item item : player.getItems())
+	    		System.out.println(item.toString());         
+	    
+	   /* System.out.println(detail.didRadianWin());
+	    System.out.println(detail.getLeagueId());
+	    System.out.println((float)detail.getDurationOfMatch()/60);
+	    
+	    GameMode mode = detail.getGameMode();
+	    System.out.println(detail.getGameMode().getValue());*/
+	    
+	   /* MatchHistory history = stats.getMostRecentMatchHistory();
+	    List<MatchOverview> overviews = history.getMatchOverviews();
+
+	    // print all match overviews found
+	    for (MatchOverview match : overviews)
+	        System.out.println(match);*/
+
+	} catch (Dota2StatsAccessException e1) {
+		e1.printStackTrace();
+	    // Do something if an error occurs
+	}
+	
+	}
+}
